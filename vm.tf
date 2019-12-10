7edbb9cbd7c60ed8158c6166e5725425de6c1ffb
qafro <qafrogce@gmail.com>
2019-12-10 21:14:58 +0000

    added Stackdriver Logging API

--- vm.tf
+++ vm.tf
@@ -26,6 +26,7 @@ resource "google_compute_instance" "default" {
   metadata_startup_script = "${file("./startup.sh")}"
 
   service_account {
-    scopes = ["userinfo-email", "compute-ro", "storage-wo"]
+    scopes = ["userinfo-email", "compute-ro", "storage-wo","Stackdriver-wo"]
   }
+   allow_stopping_for_update = true
 }
