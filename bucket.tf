5a2d330116842df93b84ddafd326050fb04942a7
qafro <qafrogce@gmail.com>
2019-12-10 16:39:28 +0000

    append bucket

--- bucket.tf
+++ bucket.tf
@@ -1,9 +1,5 @@
 resource "google_storage_bucket" "gcps3" {
-  name     = "gurus3"
+  name     = "challengegce"
   location = "EU"
 
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
 }
