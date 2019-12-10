6c6dfb4e2b75629b3f5e4071b26bd4e808b26544
qafro <qafrogce@gmail.com>
2019-12-10 21:40:17 +0000

    amended storage-wo

--- vm.tf
+++ vm.tf
@@ -26,7 +26,7 @@ resource "google_compute_instance" "default" {
   metadata_startup_script = "${file("./startup.sh")}"
 
   service_account {
-    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+    scopes = ["userinfo-email", "compute-ro", "storage-wo"]
   }
    allow_stopping_for_update = true
 }
