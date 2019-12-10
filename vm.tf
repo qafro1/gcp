0ac2dafc3f033a945672587887766815f9c661d1
qafro <qafrogce@gmail.com>
2019-12-10 22:02:36 +0000

    update

--- vm.tf
+++ vm.tf
@@ -26,7 +26,7 @@ resource "google_compute_instance" "default" {
   metadata_startup_script = "${file("./startup.sh")}"
 
   service_account {
-    scopes = ["userinfo-email","cloud-platform"]
+    scopes = ["userinfo-email","cloud-platform","https://www.googleapis.com/auth/compute.readonly"]
     #["userinfo-email", "compute-ro", "storage-ro" ]
   }
    allow_stopping_for_update = true
