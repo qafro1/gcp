38c83e3f39af37a0601a6099eed388b746b39af9
qafro <qafrogce@gmail.com>
2019-12-20 17:24:31 +0000

    added test

--- role.tf
+++ role.tf
@@ -2,5 +2,5 @@ resource "google_project_iam_custom_role" "my-custom-role" {
   role_id     = "myCustomRole"
   title       = "My Custom Role"
   description = "A description"
-  permissions = ["iam.roles.list", "iam.roles.create", "iam.roles.delete"]
+  permissions = ["iam.roles.list"]
 }
