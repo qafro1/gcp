35dfb5b72b44c165c2157a281f1f6ce1b8bbc825
qafro <qafrogce@gmail.com>
2019-12-20 17:15:03 +0000

    added custom role 201219

--- role.tf
+++ role.tf
@@ -1,5 +1,5 @@
 resource "google_project_iam_custom_role" "baserole" {
-  role_id     = "BaseGCPRole2019"
+  role_id     = "gcprole201219"
   title       = "Base GCP Role"
   description = "Custom gcp role"
   permissions = ["logging.logEntries.create", "monitoring.metricDescriptors.create", "monitoring.metricDescriptors.get","monitoring.metricDescriptors.list","monitoring.monitoredResourceDescriptors.get","monitoring.monitoredResourceDescriptors.list","monitoring.timeSeries.create"]
