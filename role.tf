723c91c9fb9fe2a80725c640f470719c1e5b345c
qafro <qafrogce@gmail.com>
2020-01-09 11:50:43 +0000

    redo logs

--- role.tf
+++ role.tf
@@ -1,6 +1,12 @@
 resource "google_project_iam_custom_role" "my-custom-role" {
-  role_id     = "BaseGceRole"
+  role_id     = "basegcerole"
   title       = "Base GCE Role"
   description = "Amend writer role to logs"
-  permissions = ["iam.roles.list", "iam.roles.create", "iam.roles.delete"]
+  permissions = ["logging.logEntries.create",
+                "monitoring.metricDescriptors.create",
+                "monitoring.metricDescriptors.get",
+                "monitoring.metricDescriptors.list",
+                "monitoring.monitoredResourceDescriptors.get",
+                "monitoring.monitoredResourceDescriptors.list",
+                "monitoring.timeSeries.create"]
 }
