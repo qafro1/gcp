c7048f15efbbcab326817bd24004b2102ee3b665
qafro <qafrogce@gmail.com>
2020-01-09 11:36:26 +0000

    added gce role

--- role.tf
+++ role.tf
@@ -1,6 +1,6 @@
 resource "google_project_iam_custom_role" "my-custom-role" {
-  role_id     = "myCustomRole"
-  title       = "My Custom Role"
-  description = "A description"
-  permissions = ["iam.roles.list"]
+  role_id     = "BaseGceRole"
+  title       = "Base GCE Role"
+  description = "Amend writer role to logs"
+  permissions = ["iam.roles.list", "iam.roles.create", "iam.roles.delete"]
 }
