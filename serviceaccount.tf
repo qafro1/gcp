c9cbfc052974aa63a06d3b73dfc923340a3f5413
qafro <qafrogce@gmail.com>
2020-01-09 14:30:48 +0000

    added data

--- serviceaccount.tf
+++ serviceaccount.tf
@@ -1,3 +1,9 @@
+data "google_project" "project" {}
+
+output "project_number" {
+  value = "${data.google_project.project.number}"
+} 
+
 resource "google_service_account" "frontend-sa" {
   account_id   = "frontend-sa"
   display_name = "frontend-sa"
@@ -6,6 +12,8 @@ resource "google_service_account" "frontend-sa" {
 resource "google_project_iam_binding" "main" {
   role    = "${google_project_iam_custom_role.main.role_id}"
   members = [
-    "user:jane@example.com",
+    "email:frontend-sa@challenge-lab-gce-261616.iam.gserviceaccount.com",
   ]
 }
+
+
