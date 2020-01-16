cddf94f581aba63a6a178b91fac1ea4f2dc6eedc
qafro <qafrogce@gmail.com>
2020-01-16 14:39:13 +0000

    updated subnet

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 
 resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
   name          = "dev-subnetwork"
-  ip_cidr_range = "10.1.0.0/16"
+  ip_cidr_range = "10.2.0.0/16"
   region        = "${var.gcp_region}"
   network       = "${google_compute_network.vpc_network.self_link}"
   secondary_ip_range {
