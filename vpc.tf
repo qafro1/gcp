f4e01bfae6e00fb684ecc777e0db1501ed2b11ee
qafro <qafrogce@gmail.com>
2020-01-16 15:00:21 +0000

    redo vpc

--- vpc.tf
+++ vpc.tf
@@ -1,15 +1,3 @@
-resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
-  name          = "test-subnetwork"
-  ip_cidr_range = "10.2.0.0/16"
-  region        = "${var.gcp_region}"
-  network       = "${google_compute_network.custom-test.self_link}"
-  secondary_ip_range {
-    range_name    = "tf-test-secondary-range-update1"
-    ip_cidr_range = "192.168.10.0/24"
-  }
-}
-
-resource "google_compute_network" "custom-test" {
-  name                    = "${var.google_compute_network}"
-  auto_create_subnetworks = false
+resource "google_compute_network" "vpc_network" {
+  name = "${var.google_compute_network}"
 }
