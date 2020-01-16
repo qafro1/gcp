42c6b7a83a7f089786f2959102840ba9d83d2e96
qafro <qafrogce@gmail.com>
2020-01-16 12:25:45 +0000

    added gce template

---
+++ gce.tf
@@ -0,0 +1,59 @@
+resource "google_compute_instance_template" "default" {
+  name        = "appserver-template"
+  description = "This template is used to create app server instances."
+
+  tags = ["dev", "vm"]
+
+  labels = {
+    environment = "dev"
+  }
+
+  instance_description = "description assigned to instances"
+  machine_type         = "${var.machine_type}"
+  can_ip_forward       = false
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  // Create a new boot disk from an image
+  disk {
+    source_image = "debian-cloud/debian-9"
+    auto_delete  = true
+    boot         = true
+  }
+
+  // Use an existing disk resource
+  disk {
+    // Instance Templates reference disks by name, not self link
+    source      = "${google_compute_disk.devvm.name}"
+    auto_delete = false
+    boot        = false
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  metadata = {
+    dev = "vm"
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+
+data "google_compute_image" "my_image" {
+  family  = "debian-9"
+  project = "${var.gcp_project}"
+}
+
+resource "google_compute_disk" "devvm" {
+  name  = "existing-disk"
+  image = "${data.google_compute_image.my_image.self_link}"
+  size  = 10
+  type  = "pd-ssd"
+  zone  = "${var.zone}"
+}
--- vars.tf
+++ vars.tf
@@ -6,3 +6,12 @@ variable "gcp_project" {
 variable "gcp_region" {
   
 }
+
+variable "machine_type" {
+  default = "f1-micro"
+}
+
+variable "zone" {
+    default = "europe-west2-b"
+  
+}
