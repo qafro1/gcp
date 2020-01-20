882ef0257321ed02f8833a1b4f59f8ad12e46a0b
qafro <qafrogce@gmail.com>
2020-01-20 13:48:33 +0000

    added service api

---
+++ service_api.tf
@@ -0,0 +1,4 @@
+resource "google_project_services" "project" {
+  project = "${var.gcp_project}"
+  services   = ["iam.googleapis.com", "cloudresourcemanager.googleapis.com","bigquery.googleapis.com"]
+}
