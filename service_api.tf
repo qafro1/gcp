191719a51e569cd6008d3a52acdb375fef7d4d8c
qafro <qafrogce@gmail.com>
2020-01-20 14:25:34 +0000

    added scc & projectsecurity api

--- service_api.tf
+++ service_api.tf
@@ -1,12 +1,12 @@
-resource "google_project_service" "project" {
-  project = "${var.gcp_project}"
-  service = "bigqueryreservation.googleapis.com"
+#resource "google_project_service" "project" {
+#  project = "${var.gcp_project}"
+#  service = "bigqueryreservation.googleapis.com"
 
-  disable_dependent_services = true
-}
+#  disable_dependent_services = true
+#}
 
 
-resource "google_scc_source" "custom_source" {
+resource "google_scc_source" "projectsecurity" {
   display_name = "devProject"
   organization = " "
   description  = "My custom Cloud Security Command Center Finding Source"
