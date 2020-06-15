81184528b0aaf12bfe01bf4c70c6c048c5ac873c
qafro1 <19574150+qafro1@users.noreply.github.com>
2020-06-15 12:49:34 +0100

    update version

--- gke.tf
+++ gke.tf
@@ -1,5 +1,5 @@
 module "kubernates" {
     source              = "./modules/gke"
     name                = "dev-gke-cluster"
-    min_master_version  = "1.15.11-gke.15"
+    min_master_version  = "1.16.9-gke.2"
 }
