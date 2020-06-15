042739b072d60d01596d4f9a20bcfe4efcdb4ae7
qafro1 <19574150+qafro1@users.noreply.github.com>
2020-06-15 12:31:35 +0100

    created input values

--- gke.tf
+++ gke.tf
@@ -1,5 +1,6 @@
 module "kubernates" {
-    source = "./modules/gke"
-    #network = module.vpc.name
-    min_master_version = "1.15.11-gke.15"
+    source              = "./modules/gke"
+    name                = "dev-gke-cluster"
+    network             = "demo-vpc-network"
+    min_master_version  = "1.15.11-gke.15"
 }
--- modules/gke/vars.tf
+++ modules/gke/vars.tf
@@ -2,13 +2,11 @@ variable "location" {
     default = "europe-west2-b"
 }
 variable "name" {
-    default = "demo-gke-cluster"
 }
 variable "machine_type" {
     default = "n1-standard-1"
 }
 variable "google_compute_network" {
-    default = "demo-vpc-network"
 }
 variable "enable_legacy_abac" {
     default = true
