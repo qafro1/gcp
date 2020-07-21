fe45a6ad8094729c57b441c83e272c08bbcf9d6a
qafro1 <19574150+qafro1@users.noreply.github.com>
2020-07-21 13:21:48 +0100

    amended CustomRoleGke

--- role.tf
+++ role.tf
@@ -1,5 +1,5 @@
 resource "google_project_iam_custom_role" "main" {
-  role_id     = "CustomRole"
+  role_id     = "CustomRoleGke"
   title       = "Base GCE Role update"
   description = "Amend writer role to logs"
   permissions = ["logging.logEntries.create","monitoring.metricDescriptors.create",
